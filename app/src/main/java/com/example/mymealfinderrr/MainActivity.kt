8551ecf47549535bec751063885ee239807080fc
kunstaytrue <st10487104@rcconnect.edu.za>
2025-03-31 23:30:00 +0200

    i created a new setcontent  , i used the (var) and remember in order to allow my users to put in input in order to get output

--- app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
+++ app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.mymealfinderrr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mymealfinderrr.ui.theme.MymealfinderrrTheme
@@ -16,31 +22,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MymealfinderrrTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            var time_of_day by remember {
+                mutableStateOf("")
+            }
+            
+            var your_meal by remember {
+                mutableStateOf("")
+            }
+            
+            Column {
+                Text(text = "MyMealFinderr")
+
+                OutlinedTextField(value = , onValueChange = )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MymealfinderrrTheme {
-        Greeting("Android")
-    }
-}
