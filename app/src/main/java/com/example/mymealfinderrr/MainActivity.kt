893aa5fb2bc3233c59a2fc8e10343f0034405230
kunstaytrue <st10487104@rcconnect.edu.za>
2025-03-31 23:42:53 +0200

    i put in a row layout in order to display my buttons in a orgarnised layout

--- app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
+++ app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -42,6 +43,10 @@ class MainActivity : ComponentActivity() {
                         Text(text = "Enter the time of day")
                     }
                 )
+
+                Row {
+                    
+                }
             }
         }
     }
