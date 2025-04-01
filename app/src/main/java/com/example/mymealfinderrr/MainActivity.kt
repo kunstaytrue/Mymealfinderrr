3bf70f1fa953ed4b557bce2c0f2bd0547f083085
kunstaytrue <st10487104@rcconnect.edu.za>
2025-04-01 15:05:09 +0200

    i added space between the app name and text field

--- app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
+++ app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mymealfinderrr.ui.theme.MymealfinderrrTheme
 
@@ -46,7 +49,9 @@ class MainActivity : ComponentActivity() {
                     fontWeight = FontWeight.Black
                 )
 
+
                 Divider()
+                Spacer(modifier = Modifier.size(30.dp))
 
                 OutlinedTextField(
                     value = time_of_day, 
