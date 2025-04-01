963a4bd5289bbc85e9a504bff1681b8e5a672e5a
kunstaytrue <st10487104@rcconnect.edu.za>
2025-04-01 14:51:38 +0200

    all i did at this point was i mad the text bolder , i made  everything more central and added a divider

--- app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
+++ app/src/main/java/com/example/mymealfinderrr/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -16,8 +17,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.mymealfinderrr.ui.theme.MymealfinderrrTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +36,17 @@ class MainActivity : ComponentActivity() {
                 mutableStateOf("")
             }
             
-            Column {
-                Text(text = "MyMealFinderr")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "MyMealFinderr",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Black
+                )
+
+                Divider()
 
                 OutlinedTextField(
                     value = time_of_day, 
